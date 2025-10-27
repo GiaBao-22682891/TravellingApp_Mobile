7bd016b41cc454068c5b661a822589c1159d43db
Bao To <you@example.com>
2025-10-27 13:40:21 +0700

    v0

---
+++ src/screens/ConfirmScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+
+const ConfirmScreen = () => {
+    return (
+        <View>
+            This is Confirm Screen
+        </View>
+    )
+}
+
+export default ConfirmScreen
---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+
+const SearchScreen = () => {
+    return (
+        <View>
+            This is Search Screen
+        </View>
+    )
+}
+
+export default SearchScreen
